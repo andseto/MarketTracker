fa0d45fe5f018b9eca756794d6b5d158891754b2
andseto <149234114+andseto@users.noreply.github.com>
2024-05-21 21:59:51 -0700

    New Main Changes
    New Main Changes

---
+++ MarketTracker/LoginPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarketTracker
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginPageRegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterPage registerPage = new RegisterPage();
+            registerPage.Show();
+            Close();
+        }
+
+        private void LoginPageLoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsernameLG.Text == "" &&  txtPasswordLG.Text == "")
+            {
+                MessageBox.Show("Enter the username and password");
+            }
+            else if (txtUsernameLG.Text == "")
+            {
+                MessageBox.Show("Enter the username");
+            }
+            else if (txtPasswordLG.Text == "")
+            {
+                MessageBox.Show("Enter the password");
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection("Data Source=ANDREW-PC\\SQLEXPRESS;Initial Catalog=LRMarketTracker;Integrated Security=True;TrustServerCertificate=True");
+                    SqlCommand cmd = new SqlCommand("select * from tbl_LoginMarketTrader where username = @username and password = @password", con);
+                    cmd.Parameters.AddWithValue("@username", txtUsernameLG.Text);
+                    cmd.Parameters.AddWithValue("@password", txtPasswordLG.Text);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        MainWindow dashboard = new MainWindow();
+                        dashboard.Show();
+                        Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("login failed");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex);
+                }
+            }
+        }
+    }
+}
---
+++ MarketTracker/RegisterPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarketTracker
+{
+    /// <summary>
+    /// Interaction logic for RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Window
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterPageRegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsernameRG.Text == "" && txtPasswordRG.Text == "")
+            {
+                MessageBox.Show("Enter the username or password");
+            }
+            else if (txtUsernameRG.Text == "")
+            {
+                MessageBox.Show("Enter the username");
+            }
+            else if (txtPasswordRG.Text == "")
+            {
+                MessageBox.Show("Enter the password");
+            }
+            else
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection("Data Source=ANDREW-PC\\SQLEXPRESS;Initial Catalog=LRMarketTracker;Integrated Security=True;TrustServerCertificate=True");
+                    con.Open();
+                    string add_data = "INSERT INTO [dbo].[tbl_LoginMarketTrader] VALUES(@username, @password)";
+                    SqlCommand cmd = new SqlCommand(add_data, con);
+                    cmd.Parameters.AddWithValue("@username", txtUsernameRG.Text);
+                    cmd.Parameters.AddWithValue("@password", txtPasswordRG.Text);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    con.Close();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Registration Completed");
+                        LoginPage loginPage = new LoginPage();
+                        loginPage.Show();
+                        Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Registration Failed");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("" + ex);
+                }
+            }
+        }
+    }
+}
